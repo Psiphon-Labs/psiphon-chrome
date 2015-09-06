5413640cf3bf6029e44efeef4cd282f0ce8af123
Mike Goldberger <mike.goldberger@gmail.com>
2015-09-06 14:31:58 -0400

    change package install path to centralize under /opt/PsiphonChrome; added post install script to copy local manifest files and set correct file ownership; initial (untested) osx target in packaging script

--- tools/create_installable_packages.sh
+++ tools/create_installable_packages.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-EXTENSION_ID=gnalljkfdmkhinjcipgjjehclbpagega
-
-# TODO: Support multiple targets, read them in from $1
-TARGET=linux
-
 BASE_PATH="$( cd "$(dirname "$0")/../" ; pwd -P )"
 cd $BASE_PATH
 
@@ -13,7 +8,8 @@ VERSION="0.0.1"
 VENDOR="Psiphon Inc."
 MAINTAINER="Psiphon Inc. <info@psiphon.ca>"
 URL="https://psiphon3.com"
-DESCRIPTIION="Chrome Native Messaging Host and Extension for using the Psiphon Circumvention System from within the browser"
+DESCRIPTION="Chrome Native Messaging Host and Extension for using the Psiphon Circumvention System from within the browser\n"
+EXTENSION_ID="gnalljkfdmkhinjcipgjjehclbpagega"
 
 MANIFEST_FILE="ca.psiphon.chrome.json"
 EXECUTABLE_NAME="psiphon-native-messaging-host"
@@ -24,9 +20,37 @@ create_external_install_preferences () {
 EOF
 }
 
-package_for_linux () {
-  create_external_install_preferences
+create_postinstall_script () {
+  cat << EOF > /tmp/post_install.sh
+#!/bin/bash
+chmod -R a+rx /opt/PsiphonChrome
 
+if [ $(uname -s) == "Linux" ]; then
+  if [ ! -d ~/.config/google-chrome/NativeMessagingHosts ]; then
+    mkdir -p ~/.config/google-chrome/NativeMessagingHosts
+  fi
+  if [ ! -d /opt/google/chrome/extensions ]; then
+    mkdir -p /opt/google/chrome/extensions
+  fi
+
+  cp /opt/PsiphonChrome/$MANIFEST_FILE ~/.config/google-chrome/NativeMessagingHosts/$MANIFEST_FILE
+  cp /opt/PsiphonChrome/${EXTENSION_ID}.json /opt/google/chrome/extensions/${EXTENSION_ID}.json
+elif [ $(uname -s) == "Darwin" ]; then
+  if [ ! -d ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts ]; then
+    mkdir -p ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts
+  fi
+  if [ ! -d ~/Library/Application\ Support/Google/Chrome/External\ Extensions ]; then
+    mkdir -p ~/Library/Application\ Support/Google/Chrome/External\ Extensions
+  fi
+
+  cp /opt/PsiphonChrome/$MANIFEST_FILE ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/$MANIFEST_FILE
+  cp /opt/PsiphonChrome/${EXTENSION_ID}.json ~/Library/Application\ Support/Google/Chrome/External\ Extensions/${EXTENSION_ID}.json
+fi
+
+EOF
+}
+
+package_for_linux () {
   BUILD_ARCHITECHTURES=( "x86_64" "i686" )
   BUILD_TARGETS=( "deb" "rpm" )
 
@@ -36,28 +60,63 @@ package_for_linux () {
       -s dir \
       -t $TARGET \
       -n $NAME \
+      --after-install /tmp/post_install.sh \
       --architecture $ARCH \
       --version $VERSION \
       --vendor "$VENDOR" \
       --maintainer "$MAINTAINER" \
       --url "$URL" \
-      --description ${DESCRIPTION} \
-      host/bin/linux/$EXECUTABLE_NAME-${ARCH}=/usr/local/bin/$EXECUTABLE_NAME \
-      host/$MANIFEST_FILE=\$HOME/.config/google-chrome/NativeMessagingHosts/$MANIFEST_FILE \
-      /tmp/${EXTENSION_ID}.json=/opt/google/chrome/extensions/${EXTENSION_ID}.json
+      --description "${DESCRIPTION}" \
+      host/bin/linux/$EXECUTABLE_NAME-$ARCH=/opt/PsiphonChrome/$EXECUTABLE_NAME \
+      host/$MANIFEST_FILE=/opt/PsiphonChrome/$MANIFEST_FILE \
+      /tmp/${EXTENSION_ID}.json=/opt/PsiphonChrome/${EXTENSION_ID}.json
     done
   done
 }
 
+package_for_osx () {
+  fpm \
+  -s dir \
+  -t osxpkg \
+  -n $NAME \
+  --after-install /tmp/post_install.sh \
+  --version $VERSION \
+  --vendor "$VENDOR" \
+  --maintainer "$MAINTAINER" \
+  --url "$URL" \
+  --description "${DESCRIPTION}" \
+  host/bin/darwin/$EXECUTABLE_NAME-x86_64=/opt/PsiphonChrome/$EXECUTABLE_NAME \
+  host/$MANIFEST_FILE=/opt/PsiphonChrome/$MANIFEST_FILE \
+  /tmp/${EXTENSION_ID}.json=/opt/PsiphonChrome/${EXTENSION_ID}.json
+}
+
 echo "Starting package creation at $(date)"
 
+TARGET=$1
 case $TARGET in
   linux)
     echo "..Target 'linux' selected. Packaging"
+    create_external_install_preferences
+    create_postinstall_script
+
     package_for_linux
     ;;
+  osx)
+    echo "..Target 'osx' selected. Packaging"
+    create_external_install_preferences
+    create_postinstall_script
+
+    package_for_osx
+    ;;
+  all)
+    echo "..Target 'all' selected. Packaging"
+    echo "...Linux"
+    package_for_linux
+    echo "...OSX"
+    package_for_osx
+    ;;
   *)
-    echo "...'linux' is the only currently supported target. Passed target was: '${TARGET}', aborting"
+    echo "Invalid target. 'linux', 'osx', and 'all' are the only currently supported targets. Passed target was: '${TARGET}', aborting"
     exit 1
     ;;
 esac
